3295c0baa464924edfb7e5d9611cdceed81d5a6e
Ana de Almeida <ana.isabel190@gmail.com>
2020-04-30 11:12:06 +0100

    intersection end with player fixed

--- script/genericLevel.js
+++ script/genericLevel.js
@@ -50,8 +50,12 @@ class GenericLevel {
 
   runLogic() {
     this.player.runLogic();
-    if ((this.player.position.y + this.player.dimensions. y > this.end.position.y) &&
-      (this.player.position.x + this.player.dimensions.x > this.end.position.x)){
+    const end = this.end;
+    //console.log(end);
+    if ((this.player.position.y + this.player.dimensions.y > end.position.y) &&
+      (this.player.position.x + this.player.dimensions.x > end.position.x) && 
+      (this.player.position.y < end.position.y + end.dimension.y) && 
+      (this.player.position.x < end.position.x + end.dimension.x)){
       //the character won
       this.levelWon = true;
     }
