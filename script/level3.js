c5169198379607870b431c17b063f1e7605f3677
Ana de Almeida <ana.isabel190@gmail.com>
2020-04-30 17:01:45 +0100

    level 3 updated

--- script/level3.js
+++ script/level3.js
@@ -4,7 +4,9 @@ class Level3 extends GenericLevel {
   
   this.end.position = {x:0, y: 250};
 
-  this.player.position = {x: 650, y: 0}
+  this.player.position = {x: 650, y: 0};
+
+  this.child.position = {x:450, y:150};
   
   for (let i=700; i <= 800; i += 50) {
     this.cars.push(new Car(this.game, { x: i, y: 400, width: 50, height: 50 }));
@@ -23,8 +25,11 @@ class Level3 extends GenericLevel {
   this.platforms.push(new Platform(this.game, {x: 750, y: 50, width: 50, height: 50}));
   
   this.platforms.push(new Platform(this.game, {x: 600, y: 300, width: 50, height: 50}));
+
   this.platforms.push(new Platform(this.game, {x: 500, y: 200, width: 50, height: 50}));
-  //this.platforms.push(new Platform(this.game, {x: 450, y: 150, width: 50, height: 50}));
+
+  this.platforms.push(new Platform(this.game, {x: 450, y: 200, width: 50, height: 50}));
+  
   this.platforms.push(new Platform(this.game, {x: 400, y: 100, width: 50, height: 50}));
   this.platforms.push(new Platform(this.game, {x: 300, y: 200, width: 100, height: 50}));
   this.platforms.push(new Platform(this.game, {x: 300, y: 0, width: 50, height: 200}));
