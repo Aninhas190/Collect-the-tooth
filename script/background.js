111ddb7444b91ea7a6930b25a2b52f73369a8f28
Ana de Almeida <ana.isabel190@gmail.com>
2020-04-30 17:29:55 +0100

    game update

--- script/background.js
+++ script/background.js
@@ -9,10 +9,10 @@ class Background {
     const context = this.game.context;
     const height = this.game.height;
     const width = this.game.width;
-    context.fillStyle = 'white';
+    context.fillStyle = '#01182A';
     context.fillRect(0, 0, width, height);
 
-    context.fillStyle = 'black';
+    context.fillStyle = 'white';
     context.font = "80px sans-serif";
     context.fillText('Game Over!', 200, 250);
     context.font = '40px sans-serif'
